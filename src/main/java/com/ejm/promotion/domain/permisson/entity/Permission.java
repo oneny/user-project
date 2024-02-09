75eec1cd5acd391675e86a5e03b144c585202622
oneny <gjwodnjs1996@gmail.com>
2024-02-09 15:36:35 +0900

    Feat: Permission entity 생

---
+++ src/main/java/com/ejm/promotion/domain/permisson/entity/Permission.java
@@ -0,0 +1,29 @@
+package com.ejm.promotion.domain.permisson.entity;
+
+import com.ejm.promotion.global.common.BaseTimeEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	private PermissionType permissionType;
+}
---
+++ src/main/java/com/ejm/promotion/domain/permisson/entity/PermissionType.java
@@ -0,0 +1,5 @@
+package com.ejm.promotion.domain.permisson.entity;
+
+public enum PermissionType {
+	SELECT, UPDATE, INSERT, DELETE
+}
