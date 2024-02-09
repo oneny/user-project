63ede9c88989de47c8b5d325aa06655e88dba123
oneny <gjwodnjs1996@gmail.com>
2024-02-09 22:17:27 +0900

    Feat: 담당자 권한 삭제 api 추가

--- src/main/java/com/ejm/promotion/domain/permisson/controller/PermissionController.java
+++ src/main/java/com/ejm/promotion/domain/permisson/controller/PermissionController.java
@@ -3,6 +3,7 @@ package com.ejm.promotion.domain.permisson.controller;
 import static com.ejm.promotion.global.common.ApiResponse.Status.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +29,18 @@ public class PermissionController {
 		@RequestBody PermissionUserDto permissionUserDto) {
 
 		permissionService.insertUserPermission(permissionId, permissionUserDto);
+
+		return ResponseEntity.ok(ApiResponse.<Void>builder()
+			.status(SUCCESS)
+			.build());
+	}
+
+	@DeleteMapping("/{permissionId}")
+	public ResponseEntity<ApiResponse<Void>> denyPermission(
+		@PathVariable Long permissionId,
+		@RequestBody PermissionUserDto permissionUserDto) {
+
+		permissionService.denyUserPermission(permissionId, permissionUserDto);
 
 		return ResponseEntity.ok(ApiResponse.<Void>builder()
 			.status(SUCCESS)
