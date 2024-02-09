2878b13150232954f092a8c68f07bdd7613f942d
oneny <gjwodnjs1996@gmail.com>
2024-02-09 16:37:24 +0900

    Feat: 신규 등록자 api 추가

---
+++ src/main/java/com/ejm/promotion/domain/user/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.ejm.promotion.domain.user.controller;
+
+import static com.ejm.promotion.global.common.ApiResponse.Status.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ejm.promotion.domain.user.dto.request.UserRegistrationDto;
+import com.ejm.promotion.domain.user.service.UserService;
+import com.ejm.promotion.global.common.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping
+	public ResponseEntity<ApiResponse<Void>> registerUser(@RequestBody UserRegistrationDto userRegistrationDto) {
+		userService.insertUser(userRegistrationDto);
+
+		return ResponseEntity.ok(ApiResponse.<Void>builder()
+			.status(SUCCESS)
+			.build());
+	}
+}
