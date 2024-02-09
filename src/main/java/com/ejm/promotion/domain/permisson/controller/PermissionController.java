92ae0421ca16333d0a02cabb49c39dd78d19754a
oneny <gjwodnjs1996@gmail.com>
2024-02-09 21:09:19 +0900

    Feat: 담당자 권한 부여 api 추가

---
+++ src/main/java/com/ejm/promotion/domain/permisson/controller/PermissionController.java
@@ -0,0 +1,36 @@
+package com.ejm.promotion.domain.permisson.controller;
+
+import static com.ejm.promotion.global.common.ApiResponse.Status.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ejm.promotion.domain.permisson.dto.PermissionUserDto;
+import com.ejm.promotion.domain.permisson.service.PermissionService;
+import com.ejm.promotion.global.common.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/permissions")
+@RequiredArgsConstructor
+public class PermissionController {
+
+	private final PermissionService permissionService;
+
+	@PostMapping("/{permissionId}")
+	public ResponseEntity<ApiResponse<Void>> enrollPermission(
+		@PathVariable Long permissionId,
+		@RequestBody PermissionUserDto permissionUserDto) {
+
+		permissionService.insertUserPermission(permissionId, permissionUserDto);
+
+		return ResponseEntity.ok(ApiResponse.<Void>builder()
+			.status(SUCCESS)
+			.build());
+	}
+}
