a8865b64c4b63007eaa73611b06e4992a5d3f53f
oneny <gjwodnjs1996@gmail.com>
2024-02-09 21:10:12 +0900

    Feat: 예외 핸들러 추가

--- src/main/java/com/ejm/promotion/global/advice/ExceptionAdvice.java
+++ src/main/java/com/ejm/promotion/global/advice/ExceptionAdvice.java
@@ -7,6 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ejm.promotion.domain.permisson.exception.NotFoundPermissionException;
+import com.ejm.promotion.domain.user.exception.NotFoundUserException;
+import com.ejm.promotion.domain.userpermission.exception.AlreadyGrantPermissionException;
+import com.ejm.promotion.domain.userpermission.exception.NotFoundUserPermissionException;
 import com.ejm.promotion.global.common.ApiResponse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +22,50 @@ public class ExceptionAdvice {
 	@ExceptionHandler(IllegalArgumentException.class)
 	protected ResponseEntity<ApiResponse<Void>> illegalArgumentException(IllegalArgumentException exception) {
 		log.info("IllegalArgumentException: {}", exception.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiResponse.<Void>builder()
+				.status(FAIL)
+				.message(exception.getMessage())
+				.build());
+	}
+
+	@ExceptionHandler(NotFoundUserException.class)
+	protected ResponseEntity<ApiResponse<Void>> notFoundUserException(NotFoundUserException exception) {
+		log.info("NotFoundUserException: {}", exception.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiResponse.<Void>builder()
+				.status(FAIL)
+				.message(exception.getMessage())
+				.build());
+	}
+
+	@ExceptionHandler(NotFoundPermissionException.class)
+	protected ResponseEntity<ApiResponse<Void>> notFoundPermissionException(NotFoundPermissionException exception) {
+		log.info("NotFoundPermissionException: {}", exception.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiResponse.<Void>builder()
+				.status(FAIL)
+				.message(exception.getMessage())
+				.build());
+	}
+
+	@ExceptionHandler(NotFoundUserPermissionException.class)
+	protected ResponseEntity<ApiResponse<Void>> notFoundUserPermissionException(NotFoundUserPermissionException exception) {
+		log.info("NotFoundUserPermissionException: {}", exception.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiResponse.<Void>builder()
+				.status(FAIL)
+				.message(exception.getMessage())
+				.build());
+	}
+
+	@ExceptionHandler(AlreadyGrantPermissionException.class)
+	protected ResponseEntity<ApiResponse<Void>> alreadyGrantPermissionException(AlreadyGrantPermissionException exception) {
+		log.info("AlreadyGrantPermissionException: {}", exception.getMessage());
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 			.body(ApiResponse.<Void>builder()
