64321efc3972c2a3989ee02419d989917a76c3fb
oneny <gjwodnjs1996@gmail.com>
2024-02-09 15:31:10 +0900

    Feat: BaseTimeEntity 생성

---
+++ src/main/java/com/ejm/promotion/global/common/BaseTimeEntity.java
@@ -0,0 +1,35 @@
+package com.ejm.promotion.global.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	public void prePersist() {
+		LocalDateTime now = LocalDateTime.now();
+		createdAt = now;
+		updatedAt = now;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = LocalDateTime.now();
+	}
+}
