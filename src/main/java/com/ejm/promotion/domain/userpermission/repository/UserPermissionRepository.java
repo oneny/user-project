ad7928c995da1fa5b5b547f8f3b5eaf3e1fb1ae2
oneny <gjwodnjs1996@gmail.com>
2024-02-09 20:33:24 +0900

    Feat: userId와 permissionId를 사용한 유저 권한 상세 조회 repository 구현

--- src/main/java/com/ejm/promotion/domain/userpermission/repository/UserPermissionRepository.java
+++ src/main/java/com/ejm/promotion/domain/userpermission/repository/UserPermissionRepository.java
@@ -1,7 +1,12 @@
 package com.ejm.promotion.domain.userpermission.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
+import com.ejm.promotion.domain.permisson.entity.Permission;
+import com.ejm.promotion.domain.user.entity.User;
 import com.ejm.promotion.domain.userpermission.entity.UserPermission;
 
 import jakarta.persistence.EntityManager;
@@ -17,5 +22,16 @@ public class UserPermissionRepository {
 
 	public void insertUserPermission(UserPermission userPermission) {
 		entityManager.persist(userPermission);
+	}
+
+	public Optional<UserPermission> findByUserIdAndPermissionId(User user, Permission permission) {
+		List<UserPermission> resultList = entityManager.createQuery(
+				"select ur from UserPermission ur where ur.user = :user and ur.permission = :permission",
+				UserPermission.class)
+			.setParameter("user", user)
+			.setParameter("permission", permission)
+			.getResultList();
+
+		return resultList.stream().findAny();
 	}
 }
