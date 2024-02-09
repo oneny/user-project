8fc47072bccf962033ef1dcda270db970c501b60
oneny <gjwodnjs1996@gmail.com>
2024-02-09 20:31:43 +0900

    Feat: 권한 상세 조회 repository 구현

--- src/main/java/com/ejm/promotion/domain/permisson/repository/PermissionRepository.java
+++ src/main/java/com/ejm/promotion/domain/permisson/repository/PermissionRepository.java
@@ -1,6 +1,7 @@
 package com.ejm.promotion.domain.permisson.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -20,5 +21,9 @@ public class PermissionRepository {
 	public List<Permission> findAll() {
 		return entityManager.createQuery("select p from Permission p", Permission.class)
 			.getResultList();
+	}
+
+	public Optional<Permission> findById(Long permissionId) {
+		return Optional.ofNullable(entityManager.find(Permission.class, permissionId));
 	}
 }
